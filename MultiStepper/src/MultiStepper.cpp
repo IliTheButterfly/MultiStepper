3a9900b853787394fd48937655834a9f519aef58
Civelier Algon <civelier.algon@gmail.com>
2021-05-27 18:43:47 -0400

    Add project files.

---
+++ MultiStepper/src/MultiStepper.cpp
@@ -0,0 +1,10 @@
+/*
+ Name:		MultiStepper.cpp
+ Created:	5/27/2021 6:43:09 PM
+ Author:	civel
+ Editor:	http://www.visualmicro.com
+*/
+
+#include "MultiStepper.h"
+
+
---
+++ MultiStepper/src/MultiStepper.h
@@ -0,0 +1,19 @@
+/*
+ Name:		MultiStepper.h
+ Created:	5/27/2021 6:43:09 PM
+ Author:	civel
+ Editor:	http://www.visualmicro.com
+*/
+
+#ifndef _MultiStepper_h
+#define _MultiStepper_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+#endif
+
